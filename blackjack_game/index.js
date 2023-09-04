49d1dcd7f6788606d1a566f5ac4e73d99df74dd1
elbence <juanjopc08@gmail.com>
2023-09-04 22:03:10 +0200

    prepared blackjack

--- blackjack_game/index.js
+++ blackjack_game/index.js
@@ -1,8 +1,3 @@
-let player = {
-    name: "Juanjo",
-    chips: 230
-}
-
 let cards = []
 let sum = 0
 let hasBlackJack = false
@@ -11,9 +6,6 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
-let playerEl = document.getElementById("player-el")
-
-playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
     let randomNumber = Math.floor( Math.random()*13 ) + 1
