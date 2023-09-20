f1afdd1277709a7e210ffbe5259f00c5019ffe7b
elbence <juanjopc08@gmail.com>
2023-09-20 11:42:34 +0200

    finished js - app working

--- unit-converter/index.js
+++ unit-converter/index.js
@@ -4,15 +4,19 @@ const lenEl = document.getElementById("len-el")
 const volEl = document.getElementById("vol-el")
 const massEl = document.getElementById("mass-el")
 
-let number = numEl.value
+numEl.addEventListener("keyup", calculateValuesAndRender)
 
-let meter_feet_string = `${number} meters = ${(number * 3.281).toFixed(3)} feet | ${number} feet = ${(number / 3.281).toFixed(3)} meters`
-let liters_gallons_string = `${number} liters = ${(number / 3.785).toFixed(3)} gallons | ${number} gallons = ${(number * 3.785).toFixed(3)} liters`
-let kilos_pounds_string = `${number} kilos = ${(number * 2.205).toFixed(3)} pounds | ${number} pounds = ${(number / 2.205).toFixed(3)} kilos`
+function calculateValuesAndRender() {   
+    let num_el_val = numEl.value
+    if (num_el_val) {
+        let number = Number(numEl.value)
 
-numEl.textContent = number
-lenEl.textContent = meter_feet_string
-volEl.textContent = liters_gallons_string
-massEl.textContent = kilos_pounds_string
+        let meter_feet_string = `${number} meters = ${(number * 3.281).toFixed(3)} feet | ${number} feet = ${(number / 3.281).toFixed(3)} meters`
+        let liters_gallons_string = `${number} liters = ${(number / 3.785).toFixed(3)} gallons | ${number} gallons = ${(number * 3.785).toFixed(3)} liters`
+        let kilos_pounds_string = `${number} kilos = ${(number * 2.205).toFixed(3)} pounds | ${number} pounds = ${(number / 2.205).toFixed(3)} kilos`
 
-numEl.addEventListener("keyup", () => {console.log("change¿? " + numEl.value + " num")})
+        lenEl.textContent = meter_feet_string
+        volEl.textContent = liters_gallons_string
+        massEl.textContent = kilos_pounds_string
+    }
+}
