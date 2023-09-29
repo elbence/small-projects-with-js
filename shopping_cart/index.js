04099752ae4c7b2d64d69bbacc0ecf4a0281791b
elbence <juanjopc08@gmail.com>
2023-09-29 10:38:42 +0200

    add shopping cart project

---
+++ shopping_cart/index.js
@@ -0,0 +1,77 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://realtime-database-f8038-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    if (inputValue) {
+        push(shoppingListInDB, inputValue)
+        clearInputFieldEl()
+    }
+})
+
+onValue(shoppingListInDB, function(snapshot) {
+    clearShoppingListEl()
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+        
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            appendItemToShoppingListEl(currentItem)
+        }    
+    } else {
+        shoppingListEl.innerHTML = "No items here... yet"
+    }
+})
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
+}
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+    
+    let newEl = document.createElement("li")
+    
+    newEl.textContent = itemValue
+    
+    newEl.addEventListener("click", function() {
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+        
+        remove(exactLocationOfItemInDB)
+    })
+    
+    shoppingListEl.append(newEl)
+}
+
+
+
+const toggleThemeBtn = document.getElementById("toggle-theme")
+const body = document.body
+
+toggleThemeBtn.addEventListener("click", () => {
+    if (body.classList.contains("dark-mode")) {
+        body.classList.remove("dark-mode")
+        toggleThemeBtn.textContent = "🌑";
+    }
+    else {
+        body.classList.add("dark-mode")
+        toggleThemeBtn.textContent = "🌞";
+    }
+})
